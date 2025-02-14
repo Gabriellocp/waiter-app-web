06c8e06c2d29efc382c878585acf9ef26687c4d1
Gabriellocp <gabriel_campista@hotmail.com>
2025-02-14 17:20:38 -0300

    feat: Implement API call on Orders

--- src/components/Orders/index.tsx
+++ src/components/Orders/index.tsx
@@ -1,33 +1,30 @@
+import { useEffect, useState } from "react";
+import { Order } from "../../types/Order";
+import { api } from "../../utils/api";
 import { OrderCard } from "../OrderCard";
 import { OrdersContainer } from "./styles";
 
 export function Orders() {
+    const [orders, setOrders] = useState<Order[]>([]);
+    useEffect(() => {
+        const fetch = async () => {
+            try {
+                const response = await api.get("/orders");
+                setOrders(response.data.orders);
+            } catch {}
+        };
+        fetch();
+    }, []);
+    const waiting = orders.filter((order) => order.status === "WAITING");
+    const production = orders.filter(
+        (order) => order.status === "IN_PRODUCTION"
+    );
+    const done = orders.filter((order) => order.status === "DONE");
     return (
         <OrdersContainer>
-            <OrderCard
-                icon="🕒"
-                title="Fila de espera"
-                orders={[
-                    {
-                        _id: "123",
-                        products: [
-                            {
-                                _id: "123",
-                                quantity: 30,
-                                product: {
-                                    imagePath: "asdas",
-                                    name: "Pizza",
-                                    price: 20,
-                                },
-                            },
-                        ],
-                        status: "WAITING",
-                        table: "123",
-                    },
-                ]}
-            />
-            <OrderCard icon="👨‍🍳" title="Preparando..." orders={[]} />
-            <OrderCard icon="✅" title="Pronto" orders={[]} />
+            <OrderCard icon="🕒" title="Fila de espera" orders={waiting} />
+            <OrderCard icon="👨‍🍳" title="Preparando..." orders={production} />
+            <OrderCard icon="✅" title="Pronto" orders={done} />
         </OrdersContainer>
     );
 }
